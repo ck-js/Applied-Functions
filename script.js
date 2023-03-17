95a494cf4627dac0a1500973dc61f7c622802915
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-03-17 15:04:28 +0700

    Add code examples of functions that apply conditionals with return statements inside of its code block. Note that if a return statement executes true then it will not run the return or print statements below it

--- script.js
+++ script.js
@@ -30,4 +30,42 @@ function getTaxedSalary(salary) {
 }
 getTaxedSalary(2500)
 
+function isGreaterThan(a, b) {
+    if (a > b) {
+        console.log(a + " is greater than " + b);
+    } else {
+        console.log(a + " is not greater than " + b);
+    }
+}
+isGreaterThan(3,7)
+
+function multiplyBy2(number) {
+    return number * 2;
+    return "This line also doesn't execute;"
+    console.log("This message won't be shown");
+}
+const displayMultiplyBy2 = multiplyBy2(10);
+console.log(displayMultiplyBy2)
+
+function getDiscount(age) {
+    if (age <= 18) {
+        return "15% discount applied";
+    } else if (age >= 65) {
+        return "40% discount for seniors";
+    }
+    return "No discount applicable";
+}
+const discount = getDiscount(69);
+console.log(discount);
+
+
+
+
+
+
+
+
+
+
+
 
